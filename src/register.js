9719a9cab026ba90e950cf90d4104cc01af73dcf
Artur Grochowski <agrochowki94@gmail.com>
2018-08-23 16:20:12 +0200

    Add custom attributes feature

--- src/register.js
+++ src/register.js
@@ -2,15 +2,18 @@ import { CognitoUserAttribute } from 'amazon-cognito-identity-js'
 
 import { getUserPool } from './config'
 
-export default function(email, password) {
+export default function(email, password, customAttributesList) {
   const userPool = getUserPool()
-
   const attributeList = [
+    ...customAttributesList.map(
+      attribute => new CognitoUserAttribute(attribute)
+    ),
     new CognitoUserAttribute({
       Name: 'email',
       Value: email
     })
   ]
+
   return new Promise((resolve, reject) => {
     userPool.signUp(email, password, attributeList, null, (err, result) => {
       if (err) {
